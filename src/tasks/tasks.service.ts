75cdc88212a83ac3714584d839395f7214b82933
MijanuL <skmijanulh@gmail.com>
2022-09-19 16:30:40 +0530

    import src/ issue fixing

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -9,7 +9,7 @@ import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './tasks.entity';
 import { Repository } from 'typeorm';
-import { User } from 'src/auth/user.entity';
+import { User } from '../auth/user.entity';
 
 @Injectable()
 export class TasksService {
